af88369a2389f93a15f75ac19176ca17dad072bc
devlopersumit <sumitsj2002@gmail.com>
2025-11-15 20:31:47 +0530

    Add useDebounce hook for search input debouncing

---
+++ client/src/hooks/useDebounce.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+
+/**
+ * Custom hook that debounces a value
+ * @param value 
+ * @param delay 
+ * @returns 
+ */
+export function useDebounce<T>(value: T, delay: number = 300): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay])
+
+  return debouncedValue
+}
+
