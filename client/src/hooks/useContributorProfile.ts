6cdddc4ae9b0e1fa2abc6ece3c709459e9af12b3
devlopersumit <sumitsj2002@gmail.com>
2025-12-25 00:02:31 +0530

    fix: Remove unused Contribution type import
    - Remove unused Contribution type from imports
    - Fix TypeScript error TS6133

--- client/src/hooks/useContributorProfile.ts
+++ client/src/hooks/useContributorProfile.ts
@@ -3,7 +3,6 @@ import {
   fetchContributorData, 
   buildContributionTimeline,
   type ContributorStats,
-  type Contribution,
   type ContributionTimelineItem
 } from '../utils/contributionTracker'
 
